5a8171e85e6c92b4bae33efbbfcf904f8a7e67ce
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2024-12-03 16:59:06 +0500

    trying to fix toast styling

--- components/markAttendanceLayout.jsx
+++ components/markAttendanceLayout.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import QRScanner from 'qr-scanner';
 import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MarkAttendance = ({ userId }) => {
   const [isMounted, setIsMounted] = useState(false);
