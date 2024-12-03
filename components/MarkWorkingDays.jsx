8ebcf5eb340289dd19995047973848e445529073
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2024-12-03 16:58:37 +0500

    trying to fix toast styling

--- components/MarkWorkingDays.jsx
+++ components/MarkWorkingDays.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AttendancePortal = () => {
 
