ab072dabcab52adbd9b83be4f820324bbcdb7796
intZaibi <shahzaibalisomroo@gmail.com>
2025-03-15 17:05:43 +0500

    maintaining Streak

--- app/api/signIn/route.js
+++ app/api/signIn/route.js
@@ -1,4 +1,5 @@
 import db from '../../../lib/db';
+import db from '../../../lib/verifyToken';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { NextResponse } from 'next/server';
@@ -63,26 +64,4 @@ export async function GET(req) {
 
 
 
-function verifyToken(req) {
-  try {
-    // Retrieve token from headers
-    const authHeader = req.headers.get('authorization');
-    const token = authHeader.split(' ')[1] || null;
-    
-    if (!token) {
-      return { error: "No token provided", status: 401 };
-    }
 
-    // Verify the token using the secret stored in environment variables
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Return the decoded user data if the token is valid
-    return { userId: decoded.id, role: decoded.role };
-  } catch (error) {
-    // Handle any errors related to token verification
-    if (error.name === 'TokenExpiredError') {
-      return { error: "Token expired", status: 401 };
-    }
-    return { error: "Invalid token", status: 401 };
-  }
-}
